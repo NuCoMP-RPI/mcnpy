1f52685381270142529e6d03eb130938612e1a2c
kowalp <kowalp@rpi.edu>
2022-12-06 13:30:02 -0500

    Installs to all envs

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
+eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 # Rebuild wheel
-cd ~/mcnp_api
+cd ~/Research/mcnp_api
 python setup.py bdist_wheel
 
 # Uninstall mcnpy
@@ -7,3 +8,14 @@ printf "y" | pip uninstall mcnpy
 
 # Install new wheel
 pip install ./dist/mcnpy-0.0.0-py3-none-any.whl
+
+# Install for openmc-env
+conda activate openmc-env
+# Uninstall metapy
+printf "y" | pip uninstall mcnpy
+
+# Install new wheel
+pip install ./dist/mcnpy-0.0.0-py3-none-any.whl
+
+# Return to base env
+conda activate base
